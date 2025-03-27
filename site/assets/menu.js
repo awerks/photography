79142d0642ab26a81f3de5de89957a48275785c1
awerks <sustila24@gmail.com>
2025-03-27 21:10:21 +0100

    Better mobile layout

--- site/assets/menu.js
+++ site/assets/menu.js
@@ -2,7 +2,7 @@ const menu = document.getElementById("menu");
 const ulMenu = document.getElementById("ulMenu");
 
 function menuToggle() {
-  menu.classList.toggle("h-36");
+  menu.classList.toggle("h-52");
 }
 
 window.addEventListener("resize", menuResize);
@@ -10,6 +10,6 @@ window.addEventListener("resize", menuResize);
 function menuResize() {
   const window_size = window.innerWidth || document.body.clientWidth;
   if (window_size > 640) {
-    menu.classList.remove("h-36");
+    menu.classList.remove("h-52");
   }
 }
