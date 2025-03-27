856d19a7d5bed40d915fda2966ce29e4ce386412
awerks <sustila24@gmail.com>
2025-03-27 18:30:05 +0100

    Refactor theme handling

--- site/assets/theme.js
+++ site/assets/theme.js
@@ -1,25 +1,17 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
-    const html = document.documentElement
-    // const themeToggle = document.querySelector("#theme-toggle");
-    const theme = localStorage.getItem("theme");
-    if (theme === "dark" || (theme !== "light" && prefersDarkScheme.matches)) {
-        html.classList.add("dark");
-        // themeToggle.checked = true;
-    } else {
-        html.classList.remove("dark");
-        // themeToggle.checked = false;
-    }
 
-});
+const theme = localStorage.getItem("theme") || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
+const html = document.documentElement;
+
+if (theme == "dark") {
+    html.classList.add("dark");
+}
+if (theme == "light") {
+    html.classList.remove("dark");
+}
+
 
 function switchTheme() {
     const html = document.documentElement;
-    if (html.classList.contains("dark")) {
-        html.classList.remove("dark");
-        localStorage.setItem("theme", "light");
-    } else {
-        html.classList.add("dark");
-        localStorage.setItem("theme", "dark");
-    }
+    html.classList.toggle("dark");
+    localStorage.setItem("theme", html.classList.contains("dark") ? "dark" : "light");
 }
